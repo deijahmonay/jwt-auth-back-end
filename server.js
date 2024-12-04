54297fd26887248cf30c755c30665a60ede02f98
deijahmonay <deijah@shack15.com>
2024-12-03 22:08:46 -0800

    add cors policy

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ dotenv.config();
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const cors = require('cors')
 
 const testJWTRouter = require('./controllers/test-jwt')
 const usersRouter = require('./controllers/users')
@@ -14,7 +15,9 @@ mongoose.connection.on('connected', () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 
+
 app.use(express.json());
+app.use(cors())
 
 // Routes go here
 app.use('/test-jwt', testJWTRouter)
